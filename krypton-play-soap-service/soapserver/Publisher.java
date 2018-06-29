4c137320ae0f4e54c44b2d23df6fb15db95a2a42
kjhdkf <adsfkjdf@gmail.com>
2018-06-29 16:52:44 +0530

    init

--- krypton-play-soap-service/soapserver/Publisher.java
+++ krypton-play-soap-service/soapserver/Publisher.java
@@ -4,6 +4,6 @@ import javax.xml.ws.Endpoint;
 
 public class Publisher {
 	public static void main(String[] args) {
-		Endpoint.publish("http://54.175.59.200:3001/ws/users", new UserControllerImpl());
+		Endpoint.publish("54.175.59.200:3001/ws/users", new UserControllerImpl());
 	}
 }
